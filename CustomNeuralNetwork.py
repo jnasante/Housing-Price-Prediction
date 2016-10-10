df685dcb27bcb1992f01e7b1139bf568a63f6a58
Shelby Vanhooser <vanhooser@ou.edu>
2016-10-10 11:39:35 -0500

    Net operational, figure included

--- CustomNeuralNetwork.py
+++ CustomNeuralNetwork.py
@@ -2,12 +2,21 @@ import numpy as np
 from data_processor import data, X, y 
 import math
 from random import shuffle
+from scipy.special import expit
+from matplotlib import pyplot as plt
 
 def sigmoid(x):
-  return 
+  output = 1.0 / (1.0 + np.exp(-x))
+  return output
 
 def sigmoidPrime(x):
-  return x * (1 - x)
+  retval = x * (1.0 - x)
+  return retval
+
+def featureScale(x):
+  means = np.mean(x, axis=0)
+  devs = np.std(x, axis=0)
+  return np.array((x - means) / devs)
 
 def shuffle_lists(a, b):
   _a = []
@@ -31,50 +40,50 @@ class NeuralNetwork():
   def predict_regression(self, samples):
     return np.dot(samples, self.weights) + self.w_0
 
-  def train_regression(self, samples, solutions, max_loops=5000, alpha=0.001):
+  def train_regression(self, samples, solutions, max_loops=1000, alpha=0.00001):
+    
     hidden_layer_size = 18
     feature_count = 18
     output_layer_size = 1
 
-    np.random.seed(10)
+    scalingFactor = 2.0
 
-    weights0 = 2.0 * np.random.random( (feature_count, hidden_layer_size) ) - 1.0 
-    weights1 = 2.0 * np.random.random( (hidden_layer_size, output_layer_size) ) - 1.0 
+    weights0 = scalingFactor * np.random.random( (feature_count, hidden_layer_size) ) - (scalingFactor / 2.0)
+    weights1 = scalingFactor * np.random.random( (hidden_layer_size, output_layer_size) ) - (scalingFactor / 2.0)
 
-    currentError = 100.0
     
-    _y = np.array([y]).T
-
-
-    prevWeights0 = weights0
-    prevWeights1 = weights1
+    errors = []
     for j in range(max_loops):
-      #print(weights0)
-      #_X, _y = shuffle_lists(X, y)
+      currentError = 0.0
+      for sam, sol in zip(samples, solutions):
+        output1 = sigmoid(np.dot(sam, weights0))
+        output2 = sigmoid(np.dot(output1, weights1))
 
-      output1 = 1 / (1 + np.exp(-1.0 * np.dot(X, weights0)))
-      output2 = 1 / (1 + np.exp(-1.0 * np.dot(output1, weights1)))
+        error = (output2 - sol) 
+        currentError += error ** 2
 
-      currentError = math.sqrt(np.sum(np.square(output2 - _y)))
-      if (j % 100) == 0:
+        delta2 = error * sigmoidPrime(output2)
+        delta1 = delta2.dot(weights1.T) * sigmoidPrime(output1)
+        
+        weights1 -= np.array([alpha * (output1 * delta2)]).T
+        weights0 -= alpha * sam.T.dot(delta1)
+
+      if j % 10 == 0:
+        errors += [currentError]
         print("At iteration %d, error : %f" % (j, math.sqrt(currentError)))
 
-      #currentError += ((y - output1) ** 2)
+    plt.plot(list(range(0, max_loops, 10)), errors)
+    plt.ylabel("SSE")
+    plt.xlabel("Iterations")
+    plt.title("Training errors vs. iteration")
+    plt.show()
+        
+      
 
-      delta2 = (output2 - _y) * sigmoidPrime(output2)
-      delta1 = delta2.dot(weights1.T) * sigmoidPrime(output1)
-
-      weights1 = weights1 - alpha * np.dot(output1.T, delta2)
-
-      #print(np.sum(weights1 - prevWeights1))
-
-      weights0 = weights0 + alpha * X.T.dot(delta1)
-
-      #print(np.sum(weights0 - prevWeights0))
-
-      #prevWeights0 = weights0
-      #prevWeights1 = weights1
-
+      
 
 nn = NeuralNetwork()
-nn.train_regression(samples=X, solutions=y)
+scaledX, scaledY = featureScale(X), featureScale(y)
+
+
+nn.train_regression(samples=scaledX, solutions=scaledY.T)
